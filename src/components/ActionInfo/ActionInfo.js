2ff01a04b04d4d88019808131cfd499255767305
MinhTriet2104 <45585341+MinhTriet2104@users.noreply.github.com>
2020-10-15 12:59:39 +0700

    Add Action Info

---
+++ src/components/ActionInfo/ActionInfo.js
@@ -0,0 +1,192 @@
+import React from "react";
+
+// styles
+import "./ActionInfo.scss";
+
+const ActionInfo = () => {
+  return (
+    <div className="ActionInfo">
+      <div className="recsToolbar">
+        <svg
+          viewBox="0 0 24 24"
+          width="24px"
+          height="24px"
+          focusable="false"
+          aria-hidden="false"
+          aria-labelledby="a5f3529b604e953b"
+          role="img"
+        >
+          <g
+            transform="translate(2 2) rotate(0 10 10)"
+            fill="none"
+            fillRule="evenodd"
+          >
+            <path
+              d="M5.368 11.041l1.227.985 1.785 1.433 1.226.984c.491.395.893.208.893-.413v-1.908c.743-.106 3.574-.444 4.198-.533.742-.106 1.29-.568 1.29-1.262v-.003c0-.695-.548-1.157-1.29-1.263-.624-.089-3.455-.427-4.198-.533V6.62c0-.621-.401-.807-.892-.413L8.38 7.19 6.595 8.624l-1.227.984c-.49.395-.49 1.04 0 1.433"
+              fill="#545454"
+              fillRule="nonzero"
+            ></path>
+            <rect
+              stroke="#545454"
+              strokeWidth="2.5"
+              width="20"
+              height="20"
+              rx="3"
+            ></rect>
+          </g>
+          <title id="a5f3529b604e953b">Left</title>
+        </svg>
+        <span className="Vam">Nope</span>
+      </div>
+      <div className="recsToolbar">
+        <svg
+          viewBox="0 0 24 24"
+          width="24px"
+          height="24px"
+          focusable="false"
+          aria-hidden="false"
+          aria-labelledby="f2b35f2437eddf8d"
+          role="img"
+        >
+          <g
+            transform="translate(2 2) rotate(180 10 10)"
+            fill="none"
+            fillRule="evenodd"
+          >
+            <path
+              d="M5.368 11.041l1.227.985 1.785 1.433 1.226.984c.491.395.893.208.893-.413v-1.908c.743-.106 3.574-.444 4.198-.533.742-.106 1.29-.568 1.29-1.262v-.003c0-.695-.548-1.157-1.29-1.263-.624-.089-3.455-.427-4.198-.533V6.62c0-.621-.401-.807-.892-.413L8.38 7.19 6.595 8.624l-1.227.984c-.49.395-.49 1.04 0 1.433"
+              fill="#545454"
+              fillRule="nonzero"
+            ></path>
+            <rect
+              stroke="#545454"
+              strokeWidth="2.5"
+              width="20"
+              height="20"
+              rx="3"
+            ></rect>
+          </g>
+          <title id="f2b35f2437eddf8d">Right</title>
+        </svg>
+        <span className="Vam">Like</span>
+      </div>
+      <div className="recsToolbar">
+        <svg
+          viewBox="0 0 24 24"
+          width="24px"
+          height="24px"
+          focusable="false"
+          aria-hidden="false"
+          aria-labelledby="bf05ff01688a54a3"
+          role="img"
+        >
+          <g
+            transform="translate(2 2) rotate(90 10 10)"
+            fill="none"
+            fillRule="evenodd"
+          >
+            <path
+              d="M5.368 11.041l1.227.985 1.785 1.433 1.226.984c.491.395.893.208.893-.413v-1.908c.743-.106 3.574-.444 4.198-.533.742-.106 1.29-.568 1.29-1.262v-.003c0-.695-.548-1.157-1.29-1.263-.624-.089-3.455-.427-4.198-.533V6.62c0-.621-.401-.807-.892-.413L8.38 7.19 6.595 8.624l-1.227.984c-.49.395-.49 1.04 0 1.433"
+              fill="#545454"
+              fillRule="nonzero"
+            ></path>
+            <rect
+              stroke="#545454"
+              strokeWidth="2.5"
+              width="20"
+              height="20"
+              rx="3"
+            ></rect>
+          </g>
+          <title id="bf05ff01688a54a3">Up</title>
+        </svg>
+        <span className="Vam">Open Profile</span>
+      </div>
+      <div className="recsToolbar">
+        <svg
+          viewBox="0 0 24 24"
+          width="24px"
+          height="24px"
+          focusable="false"
+          aria-hidden="false"
+          aria-labelledby="5dd0196c59daa441"
+          role="img"
+        >
+          <g
+            transform="translate(2 2) rotate(270 10 10)"
+            fill="none"
+            fillRule="evenodd"
+          >
+            <path
+              d="M5.368 11.041l1.227.985 1.785 1.433 1.226.984c.491.395.893.208.893-.413v-1.908c.743-.106 3.574-.444 4.198-.533.742-.106 1.29-.568 1.29-1.262v-.003c0-.695-.548-1.157-1.29-1.263-.624-.089-3.455-.427-4.198-.533V6.62c0-.621-.401-.807-.892-.413L8.38 7.19 6.595 8.624l-1.227.984c-.49.395-.49 1.04 0 1.433"
+              fill="#545454"
+              fillRule="nonzero"
+            ></path>
+            <rect
+              stroke="#545454"
+              strokeWidth="2.5"
+              width="20"
+              height="20"
+              rx="3"
+            ></rect>
+          </g>
+          <title id="5dd0196c59daa441">Down</title>
+        </svg>
+        <span className="Vam">Close Profile</span>
+      </div>
+      <div className="recsToolbar">
+        <svg
+          viewBox="0 0 32 24"
+          width="32"
+          height="24"
+          focusable="false"
+          aria-hidden="false"
+          aria-labelledby="5b2f053b48dfcf60"
+          role="img"
+        >
+          <g fill="none" fillRule="evenodd">
+            <path
+              d="M25.397 13.98H11.12v1.437c0 .575-.375.767-.939.384l-.94-.959-1.878-1.246-.94-.958c-.563-.383-.563-.958 0-1.342l.94-.958 1.879-1.342.939-.958c.564-.384.94-.288.94.383v1.533h14.277v-5.75c0-1.15.94-2.204 2.255-2.204C29.06 2 30 2.958 30 4.3v7.571c0 1.246-.94 2.3-2.254 2.3h-2.16l-.189-.191z"
+              fill="#545454"
+              fillRule="nonzero"
+            ></path>
+            <path
+              d="M2.21 5.243C2.21 3.405 3.475 2 5.159 2h13.684C20.526 2 22 3.405 22 5.243v13.514C22 20.487 20.526 22 18.842 22H5.158C3.474 22 2 20.486 2 18.757V5.243h.21z"
+              stroke="#545454"
+              strokeWidth="2.5"
+            ></path>
+          </g>
+          <title id="5b2f053b48dfcf60">Enter</title>
+        </svg>
+        <span className="Vam">Super Like</span>
+      </div>
+      <div className="recsToolbar">
+        <svg
+          viewBox="0 0 74 24"
+          width="74"
+          height="24"
+          focusable="false"
+          aria-hidden="false"
+          aria-labelledby="6f1d255ff957825f"
+          role="img"
+        >
+          <rect
+            stroke="#545454"
+            strokeWidth="2.5"
+            transform="rotate(-90 37 12)"
+            x="27"
+            y="-23"
+            width="20"
+            height="70"
+            rx="3"
+            fill="none"
+          ></rect>
+          <title id="6f1d255ff957825f">Space</title>
+        </svg>
+        <span className="Vam">Next Photo</span>
+      </div>
+    </div>
+  );
+};
+
+export default ActionInfo;
