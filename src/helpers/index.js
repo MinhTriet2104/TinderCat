7b9ed4295bbd958c7b4fb4b6c3110fc085af8b51
MinhTriet2104 <45585341+MinhTriet2104@users.noreply.github.com>
2020-10-16 12:44:29 +0700

    add functions to get data from api

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,7 @@
+import axios from "axios";
+
+const API_URL = "https://5f892e6d18c33c0016b30683.mockapi.io/";
+
 export const getDistanceFromLatLonInKm = (lat1, lon1, lat2, lon2) => {
   const deg2rad = (deg) => {
     return deg * (Math.PI / 180);
@@ -18,4 +22,24 @@ export const getDistanceFromLatLonInKm = (lat1, lon1, lat2, lon2) => {
   var d = R * c; // Distance in km
 
   return d;
+};
+
+export const getUser = async () => {
+  const user = await axios.get(API_URL + "user/1");
+  return user.data;
+};
+
+export const getCats = async () => {
+  const cats = await axios.get(API_URL + "cats");
+  return cats.data;
+};
+
+export const getLikes = async () => {
+  const likes = await axios.get(API_URL + "likes");
+  return likes.data;
+};
+
+export const getSuperLikes = async () => {
+  const superlikes = await axios.get(API_URL + "superlikes");
+  return superlikes.data;
 };
