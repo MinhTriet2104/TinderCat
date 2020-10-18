74d54c5780feddbafa374d5cfdcf7b6b222bc49d
Sun1109 <44943684+Sun1109@users.noreply.github.com>
2020-10-18 17:04:42 +0700

    Event boder-bottom under Like and SuperLike button
    Click Like -> boder-bottom Like button pink, boder-bottom SuperLike button white and contrary

--- src/components/MenuNavbar/MenuNavbar.js
+++ src/components/MenuNavbar/MenuNavbar.js
@@ -9,15 +9,19 @@ const MenuNavbar = () => {
   //Nhu
   const handleLikeClick = () => {
     document.getElementById("likeList").style.visibility = 'visible';
+    document.getElementById("MenuButtonLike").style.borderBottom = '3px solid #fc6473';
+    document.getElementById("MenuButtonSuperLike").style.borderBottom = '3px solid #fff';
   }
   //Nhu
   const handleSuperLikeClick = () => {
     document.getElementById("likeList").style.visibility = 'hidden';
+    document.getElementById("MenuButtonLike").style.borderBottom = '3px solid #fff';
+    document.getElementById("MenuButtonSuperLike").style.borderBottom = '3px solid #fc6473';
   }
   return (
     <div className="MenuNavbar">
-      <button className="MenuButton" onClick={handleLikeClick}>Like</button>
-      <button className="MenuButton" onClick={handleSuperLikeClick}>Super Like</button>
+      <button className="MenuButton" id="MenuButtonLike" onClick={handleLikeClick}>Like</button>
+      <button className="MenuButton" id="MenuButtonSuperLike" onClick={handleSuperLikeClick}>Super Like</button>
     </div>
   );
 };
