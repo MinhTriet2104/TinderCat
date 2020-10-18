7755d82b4ee14274e489f6439e9fb19a0f052bd0
logiangvuong225-20 <68698013+logiangvuong225-20@users.noreply.github.com>
2020-10-19 00:19:26 +0700

    css, link..

--- src/containers/Profile/ChangeProfile.js
+++ src/containers/Profile/ChangeProfile.js
@@ -1,72 +1,82 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Link from "../../common/CustomLink";
 
 import "./ChangeProfile.scss";
+class ChangeProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 500,
+    };
+  }
+  render() {
+    return (
+      <div className="all">
+        <div className="ChangeInfo">
+          <div className="CardMask"></div>
 
-const ChangeProfile = () => {
+          <div className="CardBody">
+            <div className="About">
+              <label htmlFor="textAbout"> GIỚI THIỆU MÈO</label>
 
-  const handleDescriptionChange = () => {}
+              <textarea
+                className="textAbout"
+                name="message"
+                rows="10"
+                cols="30"
+                maxLength="488"
+                defaultValue=""
+                onKeyPress={() =>
+                  this.setState({ count: this.state.count - 1 })
+                }
+              />
+              <p className="countText"> {this.state.count} </p>
 
-  return (
-    <div className="all">
-      <div className="ChangeInfo">
-      <div className="CardMask"></div>
+              <label htmlFor="nameAbout"> TÊN</label>
 
-        <div className="CardBody">
-          <div className="About">
-            <label htmlFor="textAbout"> GIỚI THIỆU MÈO</label>
-            
-            <textarea
-              className="textAbout"
-              name="message"
-              rows="10"
-              cols="30"
-              maxLength="500"
-              value="Sống nội tâm =)))"
-              onChange={handleDescriptionChange}
-            >
-            </textarea>
+              <input
+                name="nameAbout"
+                className="nameAbout"
+                type="text"
+                maxLength="500"
+                defaultValue="Mèo Tam thể "
+              />
 
-            <label htmlFor="nameAbout"> TÊN</label>
+              <label htmlFor="ageAbout"> TUỔI</label>
 
-            <input
-              name="nameAbout"
-              className="nameAbout"
-              type="text"
-              maxLength="500"/>
-            <label htmlFor="ageAbout"> TUỔI</label>
+              <input
+                name="ageAbout"
+                className="ageAbout"
+                type="text"
+                maxLength="500"
+                defaultValue="5"
+              />
+              <label htmlFor="hobbyAbout"> SỞ THÍCH</label>
+              <Link className="LinkH" to="/hobby">
+                <div className="hobbyAbout"> Thêm sở thích</div>
+              </Link>
+              <label htmlFor="ganderAbout"> GIỚI TÍNH</label>
+              <div className="Select_na">
+                <select className="ganderAbout">
+                  <option value={0}>Chọn:</option>
 
-            <input
-              name="ageAbout"
-              className="ageAbout"
-              type="text"
-              maxLength="500"
-            />
-            <label htmlFor="hobbyAbout"> SỞ THÍCH</label>
-            <Link className="LinkH" to="/hobby">
-            <div
-              className="hobbyAbout"
-            > Thêm sở thích</div>
-            </Link>
-            <label htmlFor="ganderAbout"> GIỚI TÍNH</label>
-            <div className="Select_na">
-              <select className="ganderAbout">
-                <option value={0}>Chọn:</option>
+                  <option value={1}>Nam</option>
+                  <option value={2}>Nữ</option>
 
-                <option value={1}>Nam</option>
-                <option value={2}>Nữ</option>
-
-                <option value={3}>Khác</option>
-              </select>
+                  <option value={3}>Khác</option>
+                </select>
+              </div>
             </div>
-          </div>
 
-        <Link to="/profile"><button className="btnSave">Lưu</button></Link> 
+            <Link to="/profile">
+              <button className="btnSave">Lưu</button>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ChangeProfile;
--- src/containers/Profile/Hobby/Hobby.js
+++ src/containers/Profile/Hobby/Hobby.js
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { Component } from 'react';
 
 import Link from "../../../common/CustomLink";
 
 import "./Hobby.scss";
-
-const Hobby = () => {
-  return (
-    <div className="SelectHobby">
+class Hobby extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+  render() {
+    return (
+      <div className="SelectHobby">
       <div className="CardMask"></div>
 
       <div className="CardBody">
         <div className="EditHobby">
           {" "}
           Sửa sở thích{" "}
-          <Link className="linkD" to="/profile">
+          <Link className="linkD" to="/change">
             <span className="Done">Xong</span>{" "}
           </Link>{" "}
         </div>
@@ -23,16 +29,18 @@ const Hobby = () => {
         </div>
         <div className="NumberHobby">
           {" "}
-          SỞ THÍCH <span className="num">(0/5)</span>{" "}
+          SỞ THÍCH <span className="num">({this.state.count}/5)</span>{" "}
         </div>
-        <button className="hobby">Đồ ăn</button>
-        <button className="hobby">Leo núi</button>
-        <button className="hobby">Thể thao điện tử</button>
-        <button className="hobby">Tour ẩm thực</button>
-        <button className="hobby">KaraoKe</button>
+        <button className="hobby" onClick={() => this.setState({ count: this.state.count + 1 })}>Đồ ăn</button>
+        <button className="hobby" onClick={() => this.setState({ count: this.state.count + 1 })}>Leo núi</button>
+        <button className="hobby" onClick={() => this.setState({ count: this.state.count + 1 })}>Thể thao điện tử</button>
+        <button className="hobby" onClick={() => this.setState({ count: this.state.count + 1 })}>Tour ẩm thực</button>
+        <button className="hobby" onClick={() => this.setState({ count: this.state.count + 1 })}>KaraoKe</button>
       </div>
     </div>
-  );
-};
+    );
+  }
+}
 
 export default Hobby;
+
