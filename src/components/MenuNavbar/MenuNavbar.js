9afc6de9382427ae5cdf6de3f7863df067ff29aa
Nguyễn Minh Triết <minhtriet2104.it@gmail.com>
2020-10-20 22:41:30 +0700

    fix MenuNavbar

--- src/components/MenuNavbar/MenuNavbar.js
+++ src/components/MenuNavbar/MenuNavbar.js
@@ -9,8 +9,18 @@ const MenuNavbar = ({ handleMenuChange, isLikeList }) => {
 
   return (
     <div className="MenuNavbar">
-      <button className={`MenuButton ${isLikeList ? 'active' : ''}`} id="MenuButtonLike" onClick={() => handleMenuChange(true)}>Like</button>
-      <button className="MenuButton" id="MenuButtonSuperLike" onClick={() => handleMenuChange(false)}>Super Like</button>
+      <button
+        className={`MenuButton ${isLikeList ? "active" : ""}`}
+        onClick={() => handleMenuChange(true)}
+      >
+        Like
+      </button>
+      <button
+        className={`MenuButton ${!isLikeList ? "active" : ""}`}
+        onClick={() => handleMenuChange(false)}
+      >
+        Super Like
+      </button>
     </div>
   );
 };
