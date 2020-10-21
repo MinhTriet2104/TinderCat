2ee58cf198905aff7b07201985c8bfec862e9083
{pltsolutions} <ntnh5600@gmail.com>
2020-10-21 07:25:50 +0700

    dialog mess

--- src/components/SuperLikeList/SuperLikeList.js
+++ src/components/SuperLikeList/SuperLikeList.js
@@ -3,10 +3,21 @@ import { useSelector } from "react-redux";
 import { Container, Row, Col } from "react-bootstrap";
 //styles
 import "./SuperLikeList.scss";
+import { Dialog, DialogTitle, TextField, DialogActions, Button } from "@material-ui/core";
+
 
 function SuperLikeList(props) {
   const superlikelist = useSelector((state) => state.superlikelist);
-  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [isOpen, setDialogIsOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setDialogIsOpen(true);
+  }
+
+  const handleClickClose = () => {
+    setDialogIsOpen(false);
+  }
+ 
   return (
     <ul className="superlikelist" id="superLikeList">
       {superlikelist.map((spLike) => (
@@ -21,12 +32,12 @@ function SuperLikeList(props) {
                   <b>{spLike.name}</b>
                 </Row>
                 <Row className="dateSuperLike">
-                  {spLike.createdAt.slice(0, 10)}
+                  {spLike.superLikesAt.slice(0, 10)}
                 </Row>
               </Col>
               <Col md={6}>
                 <button
-                  onClick={() => setModalIsOpen(true)}
+                  onClick={() => handleClickOpen()}
                   className="btn-icon"
                 >
                   <img
@@ -35,6 +46,24 @@ function SuperLikeList(props) {
                     alt="messages"
                   />
                 </button>
+                <Dialog open={isOpen} onClose={handleClickClose} aria-labelledby="form-dialog-title">
+                    <DialogTitle id="form-dialog-title">Tin nhắn</DialogTitle>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="inputMess"
+                        label={superlikelist.name}
+                        fullWidth
+                    />
+                    <DialogActions>
+                        <Button onClick={handleClickClose} color="primary">
+                            Cancel
+                        </Button>
+                        <Button onClick={handleClickClose} color="primary">
+                            Send
+                        </Button>
+                    </DialogActions>
+                </Dialog>
                 <button className="btn-icon">
                   <img
                     src="https://st2.depositphotos.com/5266903/8456/v/950/depositphotos_84568954-stock-illustration-arrow-up-flat-red-color.jpg"
